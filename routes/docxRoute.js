1c7907bedbf3c2e8d12dd1e24ee219333810f160
bsharp807 <bsharp807@gmail.com>
2020-01-13 10:59:14 +0000

    added function

--- routes/docxRoute.js
+++ routes/docxRoute.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const outputAmendedTempalte = require('./docx')
+const outputAmendedTemplate = require('./docx')
 
 router.get('/', function(req,res) {
-  
+  outputAmendedTemplate()
 })
 
 module.exports = router;
